f7c45f330c841c6a1fc4a75e86392d7e2f74ed1b
Miguel Angel Serrano <miguel.serrano.alfaro@gmail.com>
2026-04-09 21:09:42 -0600

    - Debug toolbar - métodos en WorkerProfile -Confg de visualización de tablas en panel admin -url para ver las imagenes de los trabajadores (creeeeo k funciona xd)

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import WorkerProfile, Review, Category
+
+
+
+
+
+admin.site.register(WorkerProfile)
+admin.site.register(Review)
+admin.site.register(Category)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Register your models here.
