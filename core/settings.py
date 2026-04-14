5d28a995ce7d33dbd11af072d3a6fabea371c497
kattyfer <katt12ruiz@gmail.com>
2026-04-14 12:50:45 -0600

    detalles ultimos, creo xd

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< Updated upstream
+=======
+    'debug_toolbar',
+    'servicios',
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+<<<<<<< Updated upstream
 
 # Register your models here.
+=======
+from .models import WorkerProfile, Review, Category
+
+admin.site.register(WorkerProfile)
+admin.site.register(Review)
+admin.site.register(Category)
+
+>>>>>>> Stashed changes
