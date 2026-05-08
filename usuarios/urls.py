0276107c184122b197158b19778cb677c00d135d
kattyfer <katt12ruiz@gmail.com>
2026-05-08 06:34:04 -0600

    moviendo de servicios a usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -11,4 +11,29 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
 
     path('completar_perfil/', views.completar_perfil_cliente, name='completar_perfil_cliente'),
+
+    path('review_trabajador/<int:worker_id>/', views.crear_resena, name='review_trabajador'), 
+
+    path('explorar/', views.lista_categorias_cliente, name='explorar_categorias'),
+
+    path('explorar/<int:categoria_id>/', views.trabajadores_por_categoria, name='trabajadores_categoria'),
+
+    path('perfil/completar/', views.completar_perfil_trabajador, name='completar_perfil_trabajador'),
+
+    #path('categorias/', views.lista_categorias, name='lista_categorias'),
+
+    #path('categorias/crear/', views.crear_categoria, name = 'crear_categoria'), 
+
+    #path('categorias/editar/<int:id>/', views.editar_categoria, name = 'editar_categoria'),
+
+    #path('categorias/eliminar/<int:id>', views.eliminar_categoria, name = 'eliminar_categoria'),
+
+    path('workers/', views.lista_workers, name='lista_workers'),
+
+    #path('workers/crear/', views.crear_worker, name = 'crear_worker'), 
+
+    path('workers/editar/<int:id>/', views.editar_worker, name = 'editar_worker'),
+
+    path('workers/eliminar/<int:id>', views.eliminar_worker, name = 'eliminar_worker'), 
+
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,10 @@
-from .forms import ClientProfileForm
-from django.shortcuts import render, redirect
+from .forms import ClientProfileForm, WorkerProfileForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from servicios.models import WorkerProfile, ClientProfile
-
+from django.core.exceptions import ValidationError, PermissionDenied
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -95,3 +95,62 @@ def completar_perfil_cliente(request):
     }
 
     return render(request, 'usuarios/completar_perfil.html', context)
+
+#trabajadores
+def lista_workers(request):
+    workers = WorkerProfile.objects.all()
+    return render(request, "usuarios/lista_workers.html", {"workers":workers})
+
+@login_required
+def completar_perfil_trabajador(request):
+    # 1. Obtener o crear el perfil
+    perfil, created = WorkerProfile.objects.get_or_create(user=request.user)
+    
+    # 2. Lógica de 'created': Definir un mensaje según el estado
+    if created:
+        titulo_pantalla = "¡Bienvenido! Crea tu perfil de trabajador"
+        subtitulo = "Por favor, completa tus datos para empezar a ofrecer tus servicios."
+    else:
+        titulo_pantalla = "Editar Perfil"
+        subtitulo = "Actualiza tu información profesional a continuación."
+
+    # 3. Procesamiento del Formulario   
+    if request.method == 'POST':
+        form = WorkerProfileForm(request.POST, request.FILES, instance=perfil)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = WorkerProfileForm(instance=perfil)
+    
+    context = {
+        'form': form, 
+        'titulo_pantalla': titulo_pantalla,
+        'subtitulo': subtitulo
+        }
+    return render(request,'usuarios/completar_perfil_trabajador.html', context)
+
+@login_required
+def crear_worker(request):
+    form = WorkerProfileForm(request.POST or None)
+    if form.is_valid():
+        worker = form.save(commit=False)
+        worker.user = request.user
+        worker.save()
+        return redirect("lista_workers")
+    return render(request, "usuarios/crear_worker.html",{"form":form})
+
+@login_required
+def editar_worker(request,id):
+    worker = get_object_or_404(WorkerProfile, id=id, user=request.user)
+    form = WorkerProfileForm(request.POST or None, instance=worker)
+    if form.is_valid():
+        form.save()
+        return redirect("lista_workers")
+    return render(request, "usuarios/editar_worker.html",{"form":form})
+
+@login_required
+def eliminar_worker(request, id):
+    worker = get_object_or_404(worker, id=id, user=request.user)
+    worker.delete()
+    return redirect("lista_workers")
