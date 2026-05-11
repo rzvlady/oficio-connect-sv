dce5d3c05804f15b59eec16d2e9146305b2b7a6c
Miguel Angel Serrano <miguel.serrano.alfaro@gmail.com>
2026-05-11 15:12:43 -0600

    cambio en el placeholder de  service_area en workerprofileforms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,7 +23,7 @@ class WorkerProfileForm(forms.ModelForm):
         widgets = {
             'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre completo'}),
             'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej. 7777-7777'}),
-            'service_area': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej. San Salvador, Santa Tecla...'}),
+            'service_area': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej. San Salvador...'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
             'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Escribe aquí tu experiencia...'}),
             'profile_picture': forms.FileInput(attrs={'class': 'form-control'}),
